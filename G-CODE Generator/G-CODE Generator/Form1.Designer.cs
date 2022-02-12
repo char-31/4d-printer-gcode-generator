fa6507b8711174e92ea184d5f304aae5788068b8
dsherega <dsherega@purdue.edu>
2022-02-12 18:32:25 -0500

    Initial Comments for Code
    Commented and organized code for now

--- G-CODE Generator/G-CODE Generator/Form1.Designer.cs
+++ G-CODE Generator/G-CODE Generator/Form1.Designer.cs
@@ -36,13 +36,15 @@
             this.resin_button = new System.Windows.Forms.RadioButton();
             this.top_radiobutton = new System.Windows.Forms.RadioButton();
             this.output_text = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button_generate
             // 
-            this.button_generate.Location = new System.Drawing.Point(283, 389);
+            this.button_generate.Location = new System.Drawing.Point(709, 917);
+            this.button_generate.Margin = new System.Windows.Forms.Padding(2);
             this.button_generate.Name = "button_generate";
-            this.button_generate.Size = new System.Drawing.Size(169, 52);
+            this.button_generate.Size = new System.Drawing.Size(113, 35);
             this.button_generate.TabIndex = 0;
             this.button_generate.Text = "OK";
             this.button_generate.UseVisualStyleBackColor = true;
@@ -51,9 +53,10 @@
             // base_radiobutton
             // 
             this.base_radiobutton.AutoSize = true;
-            this.base_radiobutton.Location = new System.Drawing.Point(47, 124);
+            this.base_radiobutton.Location = new System.Drawing.Point(31, 84);
+            this.base_radiobutton.Margin = new System.Windows.Forms.Padding(2);
             this.base_radiobutton.Name = "base_radiobutton";
-            this.base_radiobutton.Size = new System.Drawing.Size(172, 41);
+            this.base_radiobutton.Size = new System.Drawing.Size(119, 29);
             this.base_radiobutton.TabIndex = 1;
             this.base_radiobutton.TabStop = true;
             this.base_radiobutton.Text = "Base Layer";
@@ -63,9 +66,10 @@
             // silver_radiobutton
             // 
             this.silver_radiobutton.AutoSize = true;
-            this.silver_radiobutton.Location = new System.Drawing.Point(47, 190);
+            this.silver_radiobutton.Location = new System.Drawing.Point(31, 128);
+            this.silver_radiobutton.Margin = new System.Windows.Forms.Padding(2);
             this.silver_radiobutton.Name = "silver_radiobutton";
-            this.silver_radiobutton.Size = new System.Drawing.Size(191, 41);
+            this.silver_radiobutton.Size = new System.Drawing.Size(131, 29);
             this.silver_radiobutton.TabIndex = 2;
             this.silver_radiobutton.TabStop = true;
             this.silver_radiobutton.Text = "Silver Traces";
@@ -74,9 +78,10 @@
             // void_radiobutton
             // 
             this.void_radiobutton.AutoSize = true;
-            this.void_radiobutton.Location = new System.Drawing.Point(47, 260);
+            this.void_radiobutton.Location = new System.Drawing.Point(31, 176);
+            this.void_radiobutton.Margin = new System.Windows.Forms.Padding(2);
             this.void_radiobutton.Name = "void_radiobutton";
-            this.void_radiobutton.Size = new System.Drawing.Size(113, 41);
+            this.void_radiobutton.Size = new System.Drawing.Size(81, 29);
             this.void_radiobutton.TabIndex = 3;
             this.void_radiobutton.TabStop = true;
             this.void_radiobutton.Text = "Voids";
@@ -85,9 +90,10 @@
             // glue_radiobutton
             // 
             this.glue_radiobutton.AutoSize = true;
-            this.glue_radiobutton.Location = new System.Drawing.Point(439, 124);
+            this.glue_radiobutton.Location = new System.Drawing.Point(293, 84);
+            this.glue_radiobutton.Margin = new System.Windows.Forms.Padding(2);
             this.glue_radiobutton.Name = "glue_radiobutton";
-            this.glue_radiobutton.Size = new System.Drawing.Size(245, 41);
+            this.glue_radiobutton.Size = new System.Drawing.Size(168, 29);
             this.glue_radiobutton.TabIndex = 4;
             this.glue_radiobutton.TabStop = true;
             this.glue_radiobutton.Text = "Anisotropic Glue";
@@ -96,9 +102,10 @@
             // resin_button
             // 
             this.resin_button.AutoSize = true;
-            this.resin_button.Location = new System.Drawing.Point(439, 190);
+            this.resin_button.Location = new System.Drawing.Point(293, 128);
+            this.resin_button.Margin = new System.Windows.Forms.Padding(2);
             this.resin_button.Name = "resin_button";
-            this.resin_button.Size = new System.Drawing.Size(110, 41);
+            this.resin_button.Size = new System.Drawing.Size(78, 29);
             this.resin_button.TabIndex = 5;
             this.resin_button.TabStop = true;
             this.resin_button.Text = "Resin";
@@ -107,9 +114,10 @@
             // top_radiobutton
             // 
             this.top_radiobutton.AutoSize = true;
-            this.top_radiobutton.Location = new System.Drawing.Point(439, 260);
+            this.top_radiobutton.Location = new System.Drawing.Point(293, 176);
+            this.top_radiobutton.Margin = new System.Windows.Forms.Padding(2);
             this.top_radiobutton.Name = "top_radiobutton";
-            this.top_radiobutton.Size = new System.Drawing.Size(161, 41);
+            this.top_radiobutton.Size = new System.Drawing.Size(112, 29);
             this.top_radiobutton.TabIndex = 6;
             this.top_radiobutton.TabStop = true;
             this.top_radiobutton.Text = "Top Layer";
@@ -117,17 +125,30 @@
             // 
             // output_text
             // 
-            this.output_text.Location = new System.Drawing.Point(781, 124);
+            this.output_text.Location = new System.Drawing.Point(1295, 11);
+            this.output_text.Margin = new System.Windows.Forms.Padding(2);
             this.output_text.Multiline = true;
             this.output_text.Name = "output_text";
-            this.output_text.Size = new System.Drawing.Size(611, 460);
+            this.output_text.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.output_text.Size = new System.Drawing.Size(409, 941);
             this.output_text.TabIndex = 7;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 707);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(121, 29);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // selection_form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1424, 628);
+            this.ClientSize = new System.Drawing.Size(1715, 963);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.output_text);
             this.Controls.Add(this.top_radiobutton);
             this.Controls.Add(this.resin_button);
@@ -136,6 +157,7 @@
             this.Controls.Add(this.silver_radiobutton);
             this.Controls.Add(this.base_radiobutton);
             this.Controls.Add(this.button_generate);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "selection_form";
             this.Text = "Initial Selection";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -154,5 +176,6 @@
         private RadioButton resin_button;
         private RadioButton top_radiobutton;
         private TextBox output_text;
+        private CheckBox checkBox1;
     }
 }
--- G-CODE Generator/G-CODE Generator/Form1.cs
+++ G-CODE Generator/G-CODE Generator/Form1.cs
@@ -14,6 +14,11 @@ namespace G_CODE_Generator
             
         }
 
+        //All of your code that involves "checking if buttons are clicked" must be in "private void button_generate_Click" which will be refered to as the main body
+        //If you need to make any code that involves calculations, create a function in the proper sections below, then call them in the main body
+        //Example: In the main body I will check if the button to print a pad is called and I will use a function called "make_pad"
+        //           This function make_pad will be created outside of the main body and will involve all the calculations and outputting of text
+
         private void button_generate_Click(object sender, EventArgs e)
         {
             string pattern = @"G90";
@@ -23,10 +28,32 @@ namespace G_CODE_Generator
                 this.output_text.Text += "Found " + m_code.Value + " at position " + m_code.Index + "\r\n";
 
         }
-        private void base_radiobutton_CheckedChanged(object sender, EventArgs e)
-        {
-            this.output_text.Text += "Input G-CODE: \r\n";
-        }
+
+        //Enter your code for BASE Radio Button here
+
+        //
+
+        //Enter your code for SILVER LINES Radio Button here
+
+        //
+        //Enter your code for VOIDS Radio Button here
+
+        //
+        //Enter your code for ANISOTROPIC GLUE Radio Button here
+
+        //
+        //Enter your code for RESIN Radio Button here
+
+        //
+        //Enter your code for TOP LAYER Radio Button here
+
+        //
+
+
+
+
+
+
 
     }
 }
