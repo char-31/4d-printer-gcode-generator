d3e79a9050b8b8c39a64e14fe7f65a3f68500bb7
char-31 <94142676+char-31@users.noreply.github.com>
2022-02-17 13:50:32 -0500

    Search and Find in Text File
    Finds a specified phrase in a GCODE text file and displays where it is found

--- G-CODE Generator/G-CODE Generator/Form1.cs
+++ G-CODE Generator/G-CODE Generator/Form1.cs
@@ -1,6 +1,7 @@
 namespace G_CODE_Generator
 {
     using System;
+    using System.IO;
     using System.Text.RegularExpressions;
     public partial class selection_form : Form
     {
@@ -21,11 +22,46 @@ namespace G_CODE_Generator
 
         private void button_generate_Click(object sender, EventArgs e)
         {
-            string pattern = @"G90";
-            string input = "G1 X163 Y90 G90";
-            Match m_code = Regex.Match(input, pattern, RegexOptions.IgnoreCase);
-            if (m_code.Success)
-                this.output_text.Text += "Found " + m_code.Value + " at position " + m_code.Index + "\r\n";
+            //BASE Radio Button Code
+            if (base_radiobutton.Checked == true)
+            {
+                string line;
+                int count = 0;
+                //The pattern to find in the text
+                string pattern = @"Z:3.2";
+                try
+                {
+                    //Pass the file path and file name to the StreamReader constructor (Make sure any \ characters become \\)
+                    StreamReader sr = new StreamReader("C:\\Users\\mossc\\Documents\\~Purdue\\4D Print RESEARCH\\visual_studio_test.GCODE");
+                    //Read the first line of text
+                    line = sr.ReadLine();
+                    //Continue to read until you reach end of file
+                    while (line != null)
+                    {
+                        count += 1;
+                        Match m_code = Regex.Match(line, pattern, RegexOptions.IgnoreCase);
+                        if (m_code.Success)
+                        {
+                            this.output_text.Text += "Found " + m_code.Value + " at line " + count + " position " + (m_code.Index + 1) + "\r\n";
+                        }
+                        //Display the line to output text box (debugging only)
+                        //this.output_text.Text += line + "\r\n";
+                        //Read the next line
+                        line = sr.ReadLine();
+                    }
+                    //close the file
+                    sr.Close();
+                    Console.ReadLine();
+                }
+
+                finally
+                {
+                    this.output_text.Text += "Search Complete. \r\n";
+                }
+
+               
+            }
+           
 
         }
 
